c2f46cdc8b9af2cc46046a96e39c5d17e2f94d3f
Muaaz Ahmad <muaaz.ahmad042@gmail.com>
2025-04-11 16:31:39 +0500

    Adding smart features

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
     <div className='flex justify-between items-center lg:px-18 md:px-10 sm:px-8 px-4 w-full py-2 border-2 border-gray-200'>
         <div className='flex justify-center items-center sm:gap-3 gap-1'>
             <img src="./logo.png" alt="Logo image" className='h-16 w-16' />
-            <p className='lg:text-4xl md:text-3xl sm:text-2xl text-lg sm:font-bold'><span className='text-purple-500'>Q</span>uick <span className='text-purple-500'>D</span>igest</p>
+            <p className='lg:text-4xl md:text-3xl sm:text-2xl text-sm font-bold'><span className='text-purple-500'>Q</span>uick <span className='text-purple-500'>D</span>igest</p>
         </div>
-        <p className='lg:text-4xl md:text-3xl sm:text-2xl text-lg sm:font-bold '>AI Text Summarizer</p>
+        <p className='lg:text-4xl md:text-3xl sm:text-2xl text-sm font-bold'>AI Text Summarizer</p>
     </div>
   )
 }
